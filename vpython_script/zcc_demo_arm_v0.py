f01db6d96b68528fd4c9e9a506c80cd6ccdca292
zcc <listenzcc@mail.bnu.edu.cn>
2019-03-15 17:08:25 +0800

    modify zcc_demo_arm_v0, building 3D sketch

--- vpython_script/zcc_demo_arm_v0.py
+++ vpython_script/zcc_demo_arm_v0.py
@@ -5,64 +5,114 @@ from vpython import arrow, box, color, cylinder, radians, rate, scene, sphere, v
 
 class Arm:
     def __init__(self):
-        self.shoulder_pos = vector(0, 0, 0)
+        self.up = vector(1, 0, 0)
+        self.right = vector(0, 1, 0)
+        self.front = vector(0, 0, 1)
+        self.note_len = 20
 
-        self.upper_arm_len = 10
-        self.upper_arm_rad = 1
-        self.upper_arm_axis = vector(1, 0, 0)
-        self.upper_arm_rot = 45
+        self.sketch_rad = 3
 
-        self.small_arm_len = 10
-        self.small_arm_rad = 1
+        self.heart_pos = vector(0, 0, 0)
+        self.heart_rad = 5
+
+        self.shoulder_pos = vector(0, 30, 0)
+        self.shoulder_rad = 8
+
+        self.upper_arm_len = 40
+        self.upper_arm_rad = 5
+        self.upper_arm_axis = vector(-1, 0, 0)
+        self.upper_arm_rot = 0
+
+        self.joint_rad = 6
+
+        self.small_arm_len = 40
+        self.small_arm_rad = 5
         self.small_arm_flex = 30
 
     def init_parts(self):
-        self.shoulder = sphere(pos=self.shoulder_pos,
-                               radius=self.upper_arm_rad*2,
+        self.init_heart()
+        self.init_sketch()
+        self.init_shoulder()
+        self.init_upper_arm()
+        self.init_joint()
+        self.init_small_arm()
+
+    def init_heart(self):
+        # draw heart
+        sphere(pos=self.heart_pos, radius=self.heart_rad,
+               color=color.cyan, opacity=0.5)
+        arrow(pos=self.heart_pos, axis=self.up*self.note_len,
+              color=color.red, radius=self.sketch_rad)
+        arrow(pos=self.heart_pos, axis=self.right*self.note_len,
+              color=color.green, radius=self.sketch_rad)
+        arrow(pos=self.heart_pos, axis=self.front*self.note_len,
+              color=color.blue, radius=self.sketch_rad)
+
+    def init_sketch(self):
+        # draw sketch
+        cylinder(pos=self.heart_pos, axis=self.up, color=color.white,
+                 radius=self.sketch_rad)
+        cylinder(pos=self.heart_pos, axis=self.shoulder_pos-self.heart_pos,
+                 color=color.white, radius=self.sketch_rad)
+
+    def init_shoulder(self):
+        # draw shoulder
+        self.shoulder = sphere(pos=self.shoulder_pos, radius=self.shoulder_rad,
                                color=color.cyan, opacity=0.5)
+
+    def init_upper_arm(self):
+        # cal axis 0, 1, 2
         axis_0 = self.upper_arm_axis.norm()
         axis_1 = axis_0.cross(axis_0.rotate(1)).norm()
         axis_1.rotate(axis=axis_0, angle=radians(self.upper_arm_rot))
         axis_2 = axis_0.cross(axis_1).norm()
-        self.upper_arm_x0 = arrow(pos=self.shoulder_pos, axis=axis_0*3,
-                                  color=color.red, shaftwidth=0.2)
-        self.upper_arm_y0 = arrow(pos=self.shoulder_pos, axis=axis_1*3,
-                                  color=color.green, shaftwidth=0.2)
-        self.upper_arm_z0 = arrow(pos=self.shoulder_pos, axis=axis_2*3,
-                                  color=color.blue, shaftwidth=0.2)
-        self.upper_arm = box(pos=self.shoulder_pos+axis_0*self.upper_arm_len/2,
-                             axis=axis_0,
-                             length=self.upper_arm_len,
-                             height=self.upper_arm_rad,
-                             width=self.upper_arm_rad*1.5,
-                             color=color.cyan, opacity=0.5)
-        joint_pos = self.shoulder_pos + axis_0 * self.upper_arm_len
-        self.upper_arm_x1 = arrow(pos=joint_pos, axis=axis_0*3,
-                                  color=color.red, shaftwidth=0.2)
-        self.upper_arm_y1 = arrow(pos=joint_pos, axis=axis_1*3,
-                                  color=color.green, shaftwidth=0.2)
-        self.upper_arm_z1 = arrow(pos=joint_pos, axis=axis_2*3,
-                                  color=color.blue, shaftwidth=0.2)
 
-        self.joint = sphere(pos=joint_pos, radius=self.upper_arm_rad*1.5,
+        # draw upper_arm
+        self.upper_arm = cylinder(pos=self.shoulder_pos,
+                                  axis=axis_0 * self.upper_arm_len,
+                                  radius=self.upper_arm_rad,
+                                  color=color.cyan, opacity=0.5)
+
+        # draw xyz 0
+        self.upper_arm_x0 = arrow(pos=self.shoulder_pos, axis=axis_0*self.note_len,
+                                  color=color.red, radius=self.sketch_rad)
+        self.upper_arm_y0 = arrow(pos=self.shoulder_pos, axis=axis_1*self.note_len,
+                                  color=color.green, radius=self.sketch_rad)
+        self.upper_arm_z0 = arrow(pos=self.shoulder_pos, axis=axis_2*self.note_len,
+                                  color=color.blue, radius=self.sketch_rad)
+
+        # cal joint_pos
+        self.joint_pos = self.shoulder_pos + axis_0 * self.upper_arm_len
+
+        # draw xyz 1
+        self.upper_arm_x1 = arrow(pos=self.joint_pos, axis=axis_0*self.note_len,
+                                  color=color.red, radius=self.sketch_rad)
+        self.upper_arm_y1 = arrow(pos=self.joint_pos, axis=axis_1*self.note_len,
+                                  color=color.green, radius=self.sketch_rad)
+        self.upper_arm_z1 = arrow(pos=self.joint_pos, axis=axis_2*self.note_len,
+                                  color=color.blue, radius=self.sketch_rad)
+
+    def init_joint(self):
+        # draw joint
+        self.joint = sphere(pos=self.joint_pos, radius=self.joint_rad,
                             color=color.cyan, opacity=0.5)
-        axis_small_arm = axis_0.rotate(axis=axis_1,
-                                       angle=radians(self.small_arm_flex))
-        self.small_arm = box(pos=joint_pos+axis_small_arm*self.small_arm_len/2,
-                             axis=axis_small_arm,
-                             length=self.small_arm_len,
-                             height=self.small_arm_rad,
-                             width=self.small_arm_rad*1.5,
-                             color=color.cyan, opacity=0.5)
+
+    def init_small_arm(self):
+        # cal axis 0, 1
+        axis_0 = self.upper_arm_axis.norm()
+        axis_1 = axis_0.cross(axis_0.rotate(1)).norm()
+        axis_1.rotate(axis=axis_0, angle=radians(self.upper_arm_rot))
+
+        # draw small_arm
+        self.small_arm = cylinder(pos=self.joint_pos,
+                                  axis=axis_0 * self.small_arm_len,
+                                  radius=self.small_arm_rad,
+                                  color=color.cyan, opacity=0.5)
+        self.small_arm.rotate(axis=axis_1, angle=radians(self.small_arm_flex))
 
 
 scene.title = 'Arm v0'
-X = arrow(pos=vector(-10, -10, -10), axis=vector(5, 0, 0),
-          color=color.red, shaftwidth=0.5)
-Y = arrow(pos=vector(-10, -10, -10), axis=vector(0, 5, 0),
-          color=color.green, shaftwidth=0.5)
-Z = arrow(pos=vector(-10, -10, -10), axis=vector(0, 0, 5),
-          color=color.blue, shaftwidth=0.5)
+scene.forward = vector(0, 0, -1)
 
 arm = Arm()
 arm.init_parts()
