c1098ae379ecab6a5575d5df28b2f2b2eb31234e
zcc <listenzcc@mail.bnu.edu.cn>
2019-03-18 18:24:22 +0800

    modify matlab m files, to cut arm

--- dispObj.m
+++ dispObj.m
@@ -21,7 +21,30 @@ for i=1:size(obj.v,1)
 end
 % display object
 figure
-p=patch('vertices',obj.v,'faces',obj.f.v,'FaceVertexCData', tval);
+obj.f.v(obj.f.v==0) = nan;
+fv = [];
+c = ([-6.50, 2.40, 0.05] + [-13.56, -2.75, 1.92]) / 2;
+ll = norm(c - [-6.50, 2.40, 0.05]) * 1.1;
+for j = 1 : size(obj.f.v, 1)
+    x = obj.f.v(j, :);
+    for e = x
+        if isnan(e)
+            continue
+        end
+        if norm(obj.v(e, :) - c) < ll
+            fv = [fv; x];
+        end
+    end
+end
+% for j = 1 : size(obj.f.v, 1)
+%     fv = obj.f.v(j, :);
+%     if min(fv) == 0
+%         fv(fv==0) = max(fv);
+%     end
+%     obj.f.v(j, :) = fv;
+% end
+p=patch('vertices',obj.v,'faces', fv,'FaceVertexCData', tval);
+
 shading interp
 colormap jet;
 colormap bone;
--- test_script.m
+++ test_script.m
@@ -2,8 +2,10 @@ close all
 clear
 clc
 
-obj = readwObj(fullfile('objs', '12682_arm_v1_FINAL.obj'));
+% obj = readwObj(fullfile('objs', '12682_arm_v1_FINAL.obj'));
+
+obj = readwObj(fullfile('objs', 'malebody_BETA1.obj'));
 
 % obj = readwObj(fullfile('objs', 'isa_BP3D_4.0_obj_99', 'FJ3207.obj'));
 
-dispObj(obj)
+[verts, faces] = dispObj(obj);
