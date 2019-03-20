b54b63422a721fea84fa9b7741d43e08f2d33ab9
zcc <listenzcc@mail.bnu.edu.cn>
2019-03-20 16:43:56 +0800

    correct verts_n

--- test_script.m
+++ test_script.m
@@ -9,7 +9,7 @@ obj = readwObj(fullfile('objs', 'malebody_BETA1.obj'));
 % obj = readwObj(fullfile('objs', 'isa_BP3D_4.0_obj_99', 'FJ3207.obj'));
 
 [verts, faces] = dispObj(obj);
-
+verts_n = obj.vn;
 save(fullfile('parts', 'body_f.txt'), 'faces', '-ascii')
 save(fullfile('parts', 'body_v.txt'), 'verts', '-ascii')
-save(fullfile('parts', 'body_vn.txt'), 'verts', '-ascii')
+save(fullfile('parts', 'body_vn.txt'), 'verts_n', '-ascii')
