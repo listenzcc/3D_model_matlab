2d512e04785fd5961aca05a2cd81ae0448006f1c
zcc <listenzcc@mail.bnu.edu.cn>
2019-04-04 14:05:31 +0800

    add image 4D movie

--- psych_script/show_all_img.m
+++ psych_script/show_all_img.m
@@ -2,14 +2,67 @@ close all
 clear
 clc
 
-load all_img.mat
-all_img = uint8(all_img);
+PsychDefaultSetup(1);
+Screen('CloseAll')
+Screen('Preference', 'SkipSyncTests', 0);
 
-idx = floor(linspace(1, 75, 10));
+fixSize = 3;
+sKey = KbName('s');
 
-figure
-for j = 1 : length(idx)
-    subplot(3, 4, j)
-    imshow(all_img(:, :, :, idx(j)))
-    title(idx(j))
+screenNum = max(Screen('Screens'));
+gray = GrayIndex(screenNum);
+[windowPtr, rect] = Screen('OpenWindow', screenNum, gray);
+[x0, y0] = RectCenter(rect);
+fixRect = [x0-fixSize, y0-fixSize, x0+fixSize, y0+fixSize];
+colors = eye(3) * 255;
+for j = 1 : 3
+    Screen('FillRect', windowPtr, colors(j, :), fixRect);
+    Screen('Flip', windowPtr);
+    pause(1)
 end
+
+while 1
+    [keyIsDown, ~, keyCode] = KbCheck;
+    if keyIsDown && keyCode(sKey)
+        % start only when "s" key is pressed
+        break;
+    end
+end
+
+
+
+motion_names = {'shenchu', 'taiqi', 'waizhan', 'quzhou'}
+for mn = 1 : length(motion_names)
+    motion_name = motion_names{mn}
+    load(fullfile('movie_in_img4D', sprintf('%s.mat', motion_name)))
+    movie_4D = uint8(movie_4D);
+    sz = size(movie_4D);
+    num = sz(end)
+    
+    Screen('FillRect', windowPtr, colors(1, :), fixRect);
+    Screen('Flip', windowPtr);
+    pause(1)
+    
+    texs = nan(num, 1);
+    for j = 1 : num
+        imageTexture = Screen('MakeTexture', windowPtr, movie_4D(:, :, :, j));
+        texs(j) = imageTexture;
+    end
+    
+    for j = 1 : num
+        Screen('DrawTexture', windowPtr, texs(j), [], [], 0);
+        Screen('Flip', windowPtr);
+        pause(2/num)
+    end
+    
+    %     idx = floor(linspace(1, num, 12));
+    %     figure
+    %     for j = 1 : length(idx)
+    %         subplot(3, 4, j)
+    %         imshow(movie_4D(:, :, :, idx(j)))
+    %         title(idx(j))
+    %     end
+    %     set(gcf, 'NumberTitle', 'off', 'Name', motion_name)
+end
+
+Screen('CloseAll')
--- psych_script/show_movie_demo.m
+++ psych_script/show_movie_demo.m
@@ -2,9 +2,11 @@ close all
 clear
 clc
 
+root = pwd;
+
 PsychDefaultSetup(1);
 Screen('CloseAll')
-Screen('Preference', 'SkipSyncTests', 1);
+Screen('Preference', 'SkipSyncTests', 0);
 
 fixSize = 3;
 sKey = KbName('s');
@@ -31,40 +33,52 @@ fixRect = [x0-fixSize, y0-fixSize, x0+fixSize, y0+fixSize];
 %     end
 % end
 
-colors = eye(3) * 255;
-for j = 1 : 3
-    Screen('FillRect', windowPtr, colors(j, :), fixRect);
-    Screen('Flip', windowPtr);
-    pause(1)
-end
-
-fname = fullfile('c:\Users\liste\Documents\3D_model_matlab\movies\', 'quzhou.mp4')
-[movie, dur, ~, imgw, imgh] = Screen('OpenMovie', windowPtr, fname);
-Screen('PlayMovie', movie, dur/4);
-j = 0;
-all_img = nan(imgh+1, imgw+1, 3, 100);
-while 1
-    tex = Screen('GetMovieImage', windowPtr, movie);
-    if tex<=0
-        break;
+motion_names = {'shenchu', 'taiqi', 'waizhan', 'quzhou'}
+for mn = 1 : length(motion_names)
+    motion_name = motion_names{mn}
+    
+    colors = eye(3) * 255;
+    for j = 1 : 3
+        Screen('FillRect', windowPtr, colors(j, :), fixRect);
+        Screen('Flip', windowPtr);
+        pause(1)
     end
-    Screen('DrawTexture', windowPtr, tex);
-    Screen('Flip', windowPtr);
-    imageArray = Screen('GetImage', windowPtr);
-    j = j + 1
-    all_img(:, :, :, j) = imageArray(y0-imgh/2:y0+imgh/2, x0-imgw/2:x0+imgw/2, :);
-    Screen('Close', tex);
+    
+    fname = fullfile(root, '..', 'movies',...
+        sprintf('%s.mp4', motion_name))
+    [movie, dur, ~, imgw, imgh] = Screen('OpenMovie', windowPtr, fname);
+    Screen('PlayMovie', movie, dur/4);
+    j = 0;
+    all_img = nan(imgh+1, imgw+1, 3, 500);
+    while 1
+        tex = Screen('GetMovieImage', windowPtr, movie);
+        if tex<=0
+            break;
+        end
+        Screen('DrawTexture', windowPtr, tex);
+        Screen('Flip', windowPtr);
+        imageArray = Screen('GetImage', windowPtr);
+        j = j + 1
+        all_img(:, :, :, j) = imageArray(...
+            y0-imgh/2:y0+imgh/2, x0-imgw/2:x0+imgw/2, :);
+        Screen('Close', tex);
+    end
+    num = j;
+    all_img = all_img(:, :, :, 1:num);
+    Screen('PlayMovie', movie, 0);
+    Screen('CloseMovie', movie);
+    
+    movie_4D = uint8(all_img);
+    save(fullfile(root, 'movie_in_img4D', sprintf('%s.mat', motion_name)), 'movie_4D')
+    
+    for j = 1 : num
+        imageTexture = Screen('MakeTexture', windowPtr, all_img(:, :, :, j));
+        Screen('DrawTexture', windowPtr, imageTexture, [], [], 0);
+        Screen('Flip', windowPtr);
+        pause(4/num)
+    end
+    
 end
-num = j;
-Screen('PlayMovie', movie, 0);
-Screen('CloseMovie', movie);
-
-for j = 1 : num
-    imageTexture = Screen('MakeTexture', windowPtr, all_img(:, :, :, j));
-    Screen('DrawTexture', windowPtr, imageTexture, [], [], 0);
-    Screen('Flip', windowPtr);
-    pause(4/num)
-end
-
 Screen('CloseAll')
+
 
